cc608b64b60fcb3dacffe9fef258a85744cacbf3
Anton Kosarev <antonkosarev.dev@gmail.com>
2022-10-06 17:53:34 +0200

    Stock catalog real-time price updates via RxStomp

--- src/app/features/stock/page/stock-catalog-page/stock-catalog-page.component.ts
+++ src/app/features/stock/page/stock-catalog-page/stock-catalog-page.component.ts
@@ -1,19 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { StockService } from '../../service/stock.service';
 import { Stock } from '../../model/stock.model';
+import { Subscription } from 'rxjs';
+import { RxStompService } from '../../../../core/stomp/rx-stomp.service';
+import { StockPrice } from '../../model/stock-price.model';
 
 @Component({
   selector: 'app-stocks-page',
   templateUrl: './stock-catalog-page.component.html',
   styleUrls: ['./stock-catalog-page.component.scss']
 })
-export class StockCatalogPageComponent implements OnInit {
+export class StockCatalogPageComponent implements OnInit, OnDestroy {
 
+  private stockSubscriptions: Subscription[] = [];
   public stocks: Stock[] = [];
 
-  constructor(private stockService: StockService) { }
+  constructor(private stockService: StockService, private rxStompService: RxStompService) { }
 
   ngOnInit(): void {
-    this.stockService.getAllStocks().subscribe(stocks => this.stocks = stocks);
+    this.stockService.getAllStocks().subscribe(stocks => {
+      this.stocks = stocks;
+
+      this.stocks.forEach((stock) => {
+        const subscription = this.rxStompService.watch(`/topic/stocks/${stock.id}/prices`).subscribe(stocksPriceChange => {
+          const stockPrice: StockPrice = JSON.parse(stocksPriceChange.body);
+
+          stock.price = stockPrice.newPrice;
+        });
+
+        this.stockSubscriptions.push(subscription);
+      })
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.stockSubscriptions.forEach(subscription => subscription.unsubscribe());
   }
 }
